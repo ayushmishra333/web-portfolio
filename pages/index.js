891938c5645ae4803c1e2ae3dd7fef0395e2d7a7
ayushmishra333 <ayushmishra333@gmail.com>
2024-07-02 16:35:09 +0100

    Removed Web7 Image

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
-import web7 from "../public/web7.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);

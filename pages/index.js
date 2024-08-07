e0cde78182a5a4aff7476e2e83ab5b547ce40a67
ayushmishra333 <ayushmishra333@gmail.com>
2024-08-07 10:28:12 +0100

    Updated background text

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,6 @@ export default function Home() {
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-
                   className=" cursor-pointer text-2xl"
                 />
               </li>
@@ -49,7 +48,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 py-10">
+          <div className="text-center p-10 py-2">
             <h2 className="text-5xl py-2 text-stone-900 font-medium dark:text-neutral-300 md:text-6xl">
               Hi, I&apos;m Ayush
             </h2>
@@ -59,15 +58,22 @@ export default function Home() {
             <p className="text-md py-5 leading-8 text-stone-900 dark:text-neutral-300 max-w-xl mx-auto md:text-xl">
              Creating seamless digital experiences with a blend of creativity and technical expertise.
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <div className="text-5xl flex justify-center gap-16 py-2 text-gray-600 dark:text-gray-400">
            <a href="https://linkedin.com/in/ayushmishra333" target="_blank" rel="noreferrer"><AiFillLinkedin /></a> 
            <a href="https://github.com/ayushmishra333" target="_blank" rel="noreferrer"><AiFillGithub /></a> 
             </div>
-            <div className="mx-auto bg-gradient-to-b from-stone-50 dark:from-slate-900 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={ayush} layout="fill" objectFit="contain" />
+            <div className="relative py-2">
+            <div className="absolute inset-0 flex items-center justify-center z-0">
+              <h1 className="text-slate-900 dark:text-slate-50 text-7xl md:text-7xl lg:text-9xl font-bold opacity-20 text-center px-4">
+                SICKER THAN YOUR <span className="underline text-amber-500">AVERAGE</span>
+              </h1>
             </div>
-          </div>
-        </section>
+              <div className="mx-auto bg-gradient-to-b from-stone-50 dark:from-slate-900 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+                <Image src={ayush} layout="fill" objectFit="contain" />
+              </div>
+            </div>
+            </div>
+          </section>
         {/* <section>
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
@@ -130,7 +136,7 @@ export default function Home() {
             </div>
           </div>
         </section> */}
-        <section className="py-10">
+        <section className="py-2">
           <div> 
             <h3 className="text-3xl py-1 text-center dark:text-neutral-300 ">Portfolio</h3>
             <p className="text-md py-2 text-justify leading-8 text-gray-800 dark:text-neutral-300">
